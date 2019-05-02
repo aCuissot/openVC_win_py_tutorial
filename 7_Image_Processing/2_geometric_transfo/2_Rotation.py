5be2bbb471b836cf579dfa8d8edc05e5c762e629
aCuissot <harrypotter9752@gmail.com>
2019-05-02 14:27:22 +0200

    geometric transformations

--- 7_Image_Processing/2_geometric_transfo/2_Rotation.py
+++ 7_Image_Processing/2_geometric_transfo/2_Rotation.py
@@ -4,8 +4,6 @@ import cv2 as cv
 img = cv.imread('a.jpg', 0)
 rows, cols = img.shape
 M = cv.getRotationMatrix2D((cols / 2, rows / 2), 90, 1)
-print(img.shape[0], " ", img.shape[1])
-
 dst = cv.warpAffine(img, M, (cols, rows))
 cv.imshow('img', dst)
 cv.waitKey(0)

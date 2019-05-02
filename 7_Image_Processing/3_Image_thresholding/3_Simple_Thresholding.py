d120534ed0f778e12f90a9aaecfad5da93666999
aCuissot <harrypotter9752@gmail.com>
2019-05-02 14:54:44 +0200

    thresholding

---
+++ 7_Image_Processing/3_Image_thresholding/3_Simple_Thresholding.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv.imread('gradient.png', 0)
+_, thresh1 = cv.threshold(img, 127, 255, cv.THRESH_BINARY)  # first paramaeter is retVal, but not used in this case
+_, thresh2 = cv.threshold(img, 127, 255, cv.THRESH_BINARY_INV)
+_, thresh3 = cv.threshold(img, 127, 255, cv.THRESH_TRUNC)
+_, thresh4 = cv.threshold(img, 127, 255, cv.THRESH_TOZERO)
+_, thresh5 = cv.threshold(img, 127, 255, cv.THRESH_TOZERO_INV)
+titles = ['Original Image', 'BINARY', 'BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV']
+images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
+for i in range(6):
+    plt.subplot(2, 3, i + 1), plt.imshow(images[i], 'gray')
+    plt.title(titles[i])
+    plt.xticks([]), plt.yticks([])
+plt.show()

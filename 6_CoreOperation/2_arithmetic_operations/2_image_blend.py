78af4f76e56ccf5c3d915cbeef6ae3686c9f3c1e
aCuissot <harrypotter9752@gmail.com>
2019-05-02 12:14:06 +0200

    image basic operations

---
+++ 6_CoreOperation/2_arithmetic_operations/2_image_blend.py
@@ -0,0 +1,9 @@
+import cv2 as cv
+img1 = cv.imread('logo.png')
+img2 = cv.imread('robot.png')
+print(img1.shape)
+print(img2.shape)
+dst = cv.addWeighted(img2, 0.7, img1, 0.3, 0)
+cv.imshow('dst', dst)
+cv.waitKey(0)
+cv.destroyAllWindows()

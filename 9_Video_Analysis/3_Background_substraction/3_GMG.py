d42c1077c0c8cca0ff8bb6580967fb798cc97f1b
aCuissot <harrypotter9752@gmail.com>
2019-05-03 17:42:53 +0200

    calibration

--- 9_Video_Analysis/3_Background_substraction/3_GMG.py
+++ 9_Video_Analysis/3_Background_substraction/3_GMG.py
@@ -1,13 +1,14 @@
 import numpy as np
 import cv2 as cv
+
 cap = cv.VideoCapture('vtest.avi')
-kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE,(3,3))
+kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3, 3))
 fgbg = cv.createBackgroundSubtractorGMG()
-while(1):
+while True:
     ret, frame = cap.read()
     fgmask = fgbg.apply(frame)
     fgmask = cv.morphologyEx(fgmask, cv.MORPH_OPEN, kernel)
-    cv.imshow('frame',fgmask)
+    cv.imshow('frame', fgmask)
     k = cv.waitKey(30) & 0xff
     if k == 27:
         break
--- 9_Video_Analysis/3_Background_substraction/3_MOG.py
+++ 9_Video_Analysis/3_Background_substraction/3_MOG.py
@@ -1,11 +1,12 @@
 import numpy as np
 import cv2 as cv
+
 cap = cv.VideoCapture('vtest.avi')
 fgbg = cv.createBackgroundSubtractorMOG()
-while(1):
+while True:
     ret, frame = cap.read()
     fgmask = fgbg.apply(frame)
-    cv.imshow('frame',fgmask)
+    cv.imshow('frame', fgmask)
     k = cv.waitKey(30) & 0xff
     if k == 27:
         break

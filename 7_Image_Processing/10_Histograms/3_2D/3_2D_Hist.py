9b5fc596336457c27f2b26d26972dc72b65fcf2e
aCuissot <harrypotter9752@gmail.com>
2019-05-03 10:32:12 +0200

    Fourier

---
+++ 7_Image_Processing/10_Histograms/3_2D/3_2D_Hist.py
@@ -0,0 +1,9 @@
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+img = cv.imread('building.jpg')
+hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+hist = cv.calcHist([hsv], [0, 1], None, [180, 256], [0, 180, 0, 256])
+plt.imshow(hist, interpolation='nearest')
+plt.show()

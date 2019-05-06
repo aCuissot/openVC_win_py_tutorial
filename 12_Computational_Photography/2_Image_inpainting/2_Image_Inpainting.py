f892451183dd20b26e3586865a15895a7a4ecc5f
aCuissot <harrypotter9752@gmail.com>
2019-05-06 12:22:30 +0200

    Denoising and inpainting

---
+++ 12_Computational_Photography/2_Image_inpainting/2_Image_Inpainting.py
@@ -0,0 +1,9 @@
+import numpy as np
+import cv2 as cv
+
+img = cv.imread('old.png')
+mask = cv.imread('mask.png', 0)
+dst = cv.inpaint(img, mask, 3, cv.INPAINT_TELEA)
+cv.imshow('dst', dst)
+cv.waitKey(0)
+cv.destroyAllWindows()
